6fc15a00120138b6f0665f0ee3e4374d33cda2c1
ilhambou <82772375+ilhambou@users.noreply.github.com>
2023-12-24 15:30:28 +0100

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'firebase_options.dart'; 
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-
-Future<void> main() async{
+import 'screens/controle.page.dart';
+Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget{
-  const MyApp({super.key});
-  @override
-
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp>
-{
-
-  TextEditingController _textEditingController = TextEditingController();
-  FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text("Firebase"),
-        ),
-        body: Center(
-          child: Column(
-            children:[
-              TextField(
-                controller: _textEditingController,
-              ),
-              FilledButton(onPressed:(){
-                final user = <String, dynamic>{
-                  "Total_amount": _textEditingController.text,
-                };
-                _firebaseFirestore.collection("cash").add(user);
-
-              },child: Text("press")
-              )
-            ]
-          )
-        ),
+      home: ControlPage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
     );
   }
---
+++ lib/screens/controle.page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ControlPage extends StatelessWidget {
+  const ControlPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Gestion des utilisateurs'),
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.supervised_user_circle_sharp, size: 28,), text: 'Add'),
+              Tab(icon: Icon(Icons.edit_attributes, size: 28,), text: 'Users'),
+              Tab(icon: Icon(Icons.search, size: 28,), text: 'Search'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: const [
+            Center(child: Text('Page1'),),
+            Center(child: Text('Page2'),),
+            Center(child: Text('Page3'),),
+          ],
+        ),
+      ),
+    );
+  }
+}
