8722b6db012a3e098aa80e9d6650d0ac9a22ad87
ilhambou <82772375+ilhambou@users.noreply.github.com>
2023-12-26 17:57:42 +0100

    login

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart'; 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'screens/controle.page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -10,7 +11,7 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+/*class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
@@ -20,6 +21,176 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+    );
+  }
+
+  
+}*/
+
+class MyApp extends StatelessWidget {
+    const MyApp({Key? key}) : super(key:key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    
+    
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState  createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  //Initialize Firebase App
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.done){
+            return LoginScreen();
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+            );
+        },
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+   const LoginScreen({ Key? key}) : super(key: key);
+
+   @override
+   _LoginScreenState createState() => _LoginScreenState();
+
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+
+  //Login function
+static Future<User?> loginUsingEmailPassword({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        print("No User found for that email");
+    }
+  }
+  return user;
+}
+  @override
+  Widget build(BuildContext context) {
+    //create the textfield controller
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:  [
+         const Text("My App Title", style:TextStyle(color: Colors.black,
+          fontSize: 28.0,
+          fontWeight:  FontWeight.bold,
+          ),
+          ),
+          const Text("  Login To Your App",
+          style:  TextStyle(color: Colors.black,
+          fontSize: 44.0,
+          fontWeight: FontWeight.bold,
+          ),
+          ),
+          const SizedBox(
+            height: 44.0,
+          ),
+           TextField(
+          controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: "User Email",
+              prefixIcon:  Icon(  Icons.mail, color:Colors.black),
+            ),
+          ),
+          const SizedBox(
+            height: 26.0,
+            ),
+             TextField(
+            controller: _passwordController,
+            obscureText:  true,
+            decoration:  const  InputDecoration(
+              hintText: "User Password",
+              prefixIcon:  Icon(  Icons.lock, color:Colors.black),
+            ),
+            ),
+             const SizedBox(
+              height: 12.0,
+
+            ),
+            const Text("Don't Remember Your Password?",
+            style: TextStyle(color: Colors.blue),
+            ), 
+             const SizedBox(
+              height: 88.0,
+
+            ),
+            Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+              fillColor: const Color(0xFF0069FE),
+              elevation: 0.0,
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0)),
+           onPressed: () async {
+  User? user = await loginUsingEmailPassword(
+    email: _emailController.text,
+    password: _passwordController.text,
+    context: context,
+  );
+  print(user);
+  if (user != null) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => ControlPage()),
+    );
+  }
+},
+
+              child: const  Text("Login",style:TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              )),
+              ),
+            ),
+
+        ],
+        ),
+
     );
   }
 }
--- lib/screens/controle.page.dart
+++ lib/screens/controle.page.dart
@@ -1,4 +1,6 @@
-import 'package:crud_app/screens/allusers.page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+/*import 'package:crud_app/screens/allusers.page.dart';
 import 'package:flutter/material.dart';
 import 'package:crud_app/screens/user.add.dart';
 import 'package:crud_app/screens/allusers.page.dart';
@@ -18,7 +20,7 @@ class ControlPage extends StatelessWidget {
             tabs: [
               Tab(icon: Icon(Icons.supervised_user_circle_sharp, size: 28,), text: 'Add'),
               Tab(icon: Icon(Icons.edit_attributes, size: 28,), text: 'Users'),
-              Tab(icon: Icon(Icons.search, size: 28,), text: 'Search'),
+              Tab(icon: Icon(Icons.search, size: 28,), text: ''),
             ],
           ),
         ),
@@ -33,3 +35,52 @@ class ControlPage extends StatelessWidget {
     );
   }
 }
+*/
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:crud_app/screens/allusers.page.dart';
+import 'package:crud_app/screens/user.add.dart';
+
+class ControlPage extends StatelessWidget {
+  const ControlPage({Key? key}) : super(key: key);
+
+  // Function to handle user logout
+  Future<void> _logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pop(); // Navigate back to the login screen
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Gestion des utilisateurs'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.supervised_user_circle_sharp, size: 28,), text: 'Add'),
+              Tab(icon: Icon(Icons.edit_attributes, size: 28,), text: 'Users'),
+              Tab(icon: Icon(Icons.search, size: 28,), text: ''),
+            ],
+          ),
+          actions: [
+            // Logout button
+            IconButton(
+              icon: const Icon(Icons.exit_to_app),
+              onPressed: () => _logout(context),
+            ),
+          ],
+        ),
+        body: const TabBarView(
+          children: [
+            AddUserPage(),
+            AllUsers(),
+            Center(child: Text('Page3'),),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
